0ae4ea0dfc4850f44ba9d4bc7953008d21e9da0f
NicolasArayaB <nicolasaraya@gmail.com>
2021-02-21 18:40:07 +0000

    fetch request

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -2,10 +2,22 @@ const getState = ({ getStore, setStore }) => {
 	return {
 		store: {
 			//Your data structures, A.K.A Entities
+			contactsList: []
 		},
 		actions: {
 			//(Arrow) Functions that update the Store
 			// Remember to use the scope: scope.state.store & scope.setState()
+			getContacts: async () => {
+				const settings = {
+					method: "GET",
+					headers: { "Content-Type": "application/json" }
+                };
+                
+                const request = await fetch("https://assets.breatheco.de/apis/fake/contact/", settings);
+                const json = await request.json();
+                const data = json;
+                setStore({ contactsList: data})
+			}
 		}
 	};
 };
