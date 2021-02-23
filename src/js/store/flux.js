b5f812b3f86ebacb6bb6e90159a64c5217459982
NicolasArayaB <nicolasaraya@gmail.com>
2021-02-23 12:55:46 +0000

    api calls on flux.js

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -7,16 +7,90 @@ const getState = ({ getStore, setStore }) => {
 		actions: {
 			//(Arrow) Functions that update the Store
 			// Remember to use the scope: scope.state.store & scope.setState()
-			getContacts: async () => {
+			getAgenda: async () => {
 				const settings = {
 					method: "GET",
 					headers: { "Content-Type": "application/json" }
-                };
-                
-                const request = await fetch("https://assets.breatheco.de/apis/fake/contact/", settings);
-                const json = await request.json();
-                const data = json;
-                setStore({ contactsList: data})
+				};
+
+				const request = await fetch("https://assets.breatheco.de/apis/fake/contact/agenda/nico", settings);
+				const json = await request.json();
+				const data = json;
+				console.log(data, "<--- get agenda");
+				setStore({ agenda: data });
+			},
+
+			// Get a particular contact by id.
+			getContact: async id => {
+				const settings = {
+					method: "GET",
+					headers: { "Content-Type": "application/json" }
+				};
+
+				const request = await fetch(`https://assets.breatheco.de/apis/fake/contact${id}`, settings);
+				const json = await request.json;
+				const data = json;
+				console.log(data, "<--- get contact");
+				setStore({ contact: data });
+			},
+
+			// delete 1 particular contact by id
+			deleteContact: async id => {
+				const request = await fetch(`https://assets.breatheco.de/apis/fake/contact/${id}`, {
+					method: "DELETE"
+				});
+				const json = await request.json();
+				const data = json;
+				console.log(data, "<--- delete contact");
+			},
+
+			// delete all contacts (agenda)
+			deleteAgenda: async () => {
+				const request = await fetch(`https://assets.breatheco.de/apis/fake/contact/agenda/nico`, {
+					method: "DELETE"
+				});
+				const json = await request.json();
+				const data = json;
+				console.log(data, "<--- delete agenda");
+			},
+
+			createContact: async info => {
+				console.log(info);
+				const settings = {
+					method: "POST",
+					headers: { "Content-Type": "application/json" },
+					body: {
+						full_name: "Dave Bradley",
+						email: "dave@gmail.com",
+						agenda_slug: "nico",
+						address: "47568 NW 34ST, 33434 FL, USA",
+						phone: "7864445566"
+					}
+				};
+
+				const request = await fetch("https://assets.breatheco.de/apis/fake/contact/", settings);
+				const json = await request.json;
+				const data = json;
+				console.log(data, "<--- create contact");
+			},
+
+			// update contact info
+			updateContact: async id => {
+				const settings = {
+					method: "PUT",
+					headers: { "Content-Type": "application/json" },
+					body: {
+						full_name: "Dave Bradley",
+						email: "dave@gmail.com",
+						agenda_slug: "nico",
+						address: "47568 NW 34ST, 33434 FL, USA",
+						phone: "7864445566"
+					}
+				};
+				const request = await fetch(`https://assets.breatheco.de/apis/fake/contact/${id}`, settings);
+				const json = await request.json();
+				const data = json;
+				console.log(data, "<--- update contact");
 			}
 		}
 	};
